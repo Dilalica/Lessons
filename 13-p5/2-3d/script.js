ae7606cf0cb0c1cdda08ec7aae220a2f014745bc
l-ct <hola@l-ct.co>
2020-06-12 22:23:08 -0400

    p5 3d

--- 13-p5/2-3d/script.js
+++ 13-p5/2-3d/script.js
@@ -1,11 +1,12 @@
-let positions = [];
-const size = 10;
+let boxes = [];
+const volume = 10;
+const quantity = 1000;
 
 function setup(){
     createCanvas(windowWidth, windowHeight, WEBGL);
     // normalMaterial();
-    for(let i=0; i<1000; i++){
-        positions.push({
+    for(let i=0; i<quantity; i++){
+        boxes.push({
             x: randomGaussian(0, windowWidth / 5),
             y: randomGaussian(0, windowHeight / 5),
             z: randomGaussian(-50, 300),
@@ -13,7 +14,7 @@ function setup(){
             rotY: random(0, 0.1)
         });
     }
-    console.log(positions);
+    console.log(boxes);
     noStroke();
     ambientMaterial(255);
 }
@@ -22,12 +23,12 @@ function draw(){
     background(255);
     pointLight(255, 255, 255, 0, 0, 300);
 
-    positions.forEach(pos => {
+    boxes.forEach(b => {
         push();
-        translate(pos.x, pos.y, pos.z);
-        rotateX(frameCount * pos.rotX);
-        rotateY(frameCount * pos.rotY);
-        box(size);
+        translate(b.x, b.y, b.z);
+        rotateX(frameCount * b.rotX);
+        rotateY(frameCount * b.rotY);
+        box(volume);
         pop();
     });
 }
